c8a7d8c5432e1ffd2415156a712301ae2a633e8a
shubham gupta <shubham@blr1.cdac.in>
2024-06-25 17:41:40 +0530

    add outlet to render the pages

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const AppLayout = () => {
   return (
     <div>
       <main>
-        
+        <Outlet/>
       </main>
     </div>
   )
