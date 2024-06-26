1feb409398861b462ad984cac4b1a46e3c927b4a
shubham gupta <shubham@blr1.cdac.in>
2024-06-26 12:29:31 +0530

    complete ui

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { BeatLoader } from "react-spinners";
 
 const Login = () => {
   return (
-    <div>
-      Login
-    </div>
-  )
-}
+    <Card>
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>
+          to your account if you already have one
+        </CardDescription>
+      </CardHeader>
+      <CardContent classname="space-y-2">
+        <div className="space-y-1">
+          <Input name="email" type="email" placeholder="Enter Email" />
+        </div>
+        <div className="space-y-1">
+          <Input name="password" type="email" placeholder="Enter Password" />
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button>
+          {true ? <BeatLoader size={10} color="#36d7b7" /> : "Login"}
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
 
-export default Login
+export default Login;
