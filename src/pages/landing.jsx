604dfbe05a5bad4267020b816a26b09c63d7f679
shubham gupta <shubham@blr1.cdac.in>
2024-06-26 11:34:24 +0530

    add accordian in landing page

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,6 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const LandingPage = () => {
   return (
@@ -17,9 +23,41 @@ const LandingPage = () => {
           placeholder="Enter your long URL"
           className="h-full flex-1 py-4 px-4"
         />
-        <Button type="submit" className="h-full" variant="destructive">Shorten!</Button>
+        <Button type="submit" className="h-full" variant="destructive">
+          Shorten!
+        </Button>
       </form>
       <img src="/banner.jpeg" alt="banner" className="w-full my-11 md:px-11" />
+      <Accordion type="multiple" collapsible className="w-full md:px-11">
+        <AccordionItem value="item-1">
+          <AccordionTrigger>
+            How does the Trimrr URL shortener works?
+          </AccordionTrigger>
+          <AccordionContent>
+            When you enter a long URL, our system generates a shorter version of
+            that URL. This shortened URL redirects to the original long URL when
+            accessed.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>
+            Do I need an account to use the app?
+          </AccordionTrigger>
+          <AccordionContent>
+            Yes. Creating an account allows you to manage your URLs, view
+            analytics, and customize your short URLs.
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>
+            What analytics are available for my shortened URLs?
+          </AccordionTrigger>
+          <AccordionContent>
+            You can view the number of clicks, geolocation data of the clicks
+            and device types (mobile/desktop) for each of your shortened URLs.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 };
