a2e9b053c222ba9d42298e422207e38fb9dc9bb0
shubham gupta <shubham@blr1.cdac.in>
2024-06-26 12:02:33 +0530

    add heading and search params

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const Auth = () => {
-  return (
-    <div>
-      Auth
-    </div>
-  )
-}
+  const [searchParams] = useSearchParams();
 
-export default Auth
+  return (
+    <div className="mt-36 flex flex-col items-center gap-10">
+      <h1 className="text-5xl font-extrabold">
+        {searchParams.get("createNew")
+          ? "Hold up! Let's login first.."
+          : "Login / Signup"}
+      </h1>
+    </div>
+  );
+};
+
+export default Auth;
