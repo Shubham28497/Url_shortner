efa45cb0a088effa925e55b7fe917e2e7b9f138a
shubham gupta <shubhamgupta24640020@gmail.com>
2024-06-27 11:21:22 +0530

    add curr user func

--- src/db/apiAuth.js
+++ src/db/apiAuth.js
@@ -8,3 +8,13 @@ const{data,error}=await supabase.auth.signInWithPassword({
 if(error) throw new Error(error.message)
 return data;
 }
+
+export async function getCurrentUser() {
+    const {data: session, error} = await supabase.auth.getSession();
+    if (!session.session) return null;
+  
+    // const {data, error} = await supabase.auth.getUser();
+  
+    if (error) throw new Error(error.message);
+    return session.session?.user;
+  }
