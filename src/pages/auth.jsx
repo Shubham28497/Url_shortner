63b12c88760dfa44c16f571691d1039c17ca8f2d
shubham gupta <shubhamgupta24640020@gmail.com>
2024-06-28 09:28:51 +0530

    add auth

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,11 +1,22 @@
-import React from "react";
-import { useSearchParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Login from "@/components/login";
 import Signup from "@/components/signup";
+import { UrlState } from "@/context";
 
 const Auth = () => {
   const [searchParams] = useSearchParams();
+  const longLink=searchParams.get("createNew")
+  const navigate=useNavigate()
+
+  const {isAuthenticated,loading}=UrlState()
+
+  useEffect(()=>{
+    if(isAuthenticated && !loading){
+      navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
+    }
+  },[isAuthenticated,loading])
 
   return (
     <div className="mt-36 flex flex-col items-center gap-10">
