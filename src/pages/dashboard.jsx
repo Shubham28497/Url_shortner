b282c70dac9784224db513b816442ac3695fb196
shubham gupta <shubhamgupta24640020@gmail.com>
2024-07-03 10:15:00 +0530

    add link btn and filter

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BarLoader } from "react-spinners";
 import {
   Card,
@@ -8,8 +8,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Filter } from "lucide-react";
 
 const Dashboard = () => {
+  const [searchQuery,setSearchQuery]=useState("")
   return (
     <div className="flex flex-col gap-8">
       {true && <BarLoader width={"120%"} color="#36d7b7" />}
@@ -30,6 +34,17 @@ const Dashboard = () => {
             <p>0</p>
           </CardContent>
         </Card>
+      </div>
+      <div className="flex justify-between">
+        <h1 className="text-4xl font-extrabold">My links</h1>
+        <Button>Create Link</Button>
+      </div>
+      <div className="relative">
+      <Input type="text" placeholder="Filter Links..."
+      value={searchQuery}
+      onChange={(e)=>setSearchQuery(e.target.value)}
+      />
+      <Filter className="absolute top-2 right-2"/>
       </div>
     </div>
   );
