1ac5e664b31c18aeba7185d5dd93e95d79adf78d
shubham gupta <shubhamgupta24640020@gmail.com>
2024-07-03 10:00:47 +0530

    add cards

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { BarLoader } from "react-spinners";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
+    <div className="flex flex-col gap-8">
+      {true && <BarLoader width={"120%"} color="#36d7b7" />}
+      <div className="grid grid-cols-2 gap-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>Links created</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>0</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Total click</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>0</p>
+          </CardContent>
+        </Card>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
