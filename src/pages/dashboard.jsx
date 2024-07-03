c00339bb866dcf1d92af129b72645713f48d64bb
shubham gupta <shubhamgupta24640020@gmail.com>
2024-07-03 10:26:11 +0530

    call api's

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { BarLoader } from "react-spinners";
 import {
   Card,
@@ -11,12 +11,38 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Filter } from "lucide-react";
+import Error from "@/components/error";
+import useFetch from "@/hooks/usefetch";
+import { UrlState } from "@/context";
+import { getUrls } from "@/db/apiUrl";
+import { getClicksForUrls } from "@/db/apiClicks";
 
 const Dashboard = () => {
-  const [searchQuery,setSearchQuery]=useState("")
+  const [searchQuery, setSearchQuery] = useState("");
+  const { user } = UrlState();
+  const { loading, error, data: urls, fn: fnUrls } = useFetch(getUrls, user.id);
+  const {
+    loading: loadingClicks,
+    data: clicks,
+    fn: fnClicks,
+  } = useFetch(
+    getClicksForUrls,
+    urls?.map((url) => url.id)
+  );
+  useEffect(() => {
+    fnUrls();
+  }, []);
+
+  const filteredUrls = urls?.filter((url) =>
+    url.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  useEffect(() => {
+    if (urls?.length) fnClicks();
+  }, [urls?.length]);
   return (
     <div className="flex flex-col gap-8">
-      {true && <BarLoader width={"120%"} color="#36d7b7" />}
+      {loading || loadingClicks && <BarLoader width={"120%"} color="#36d7b7" />}
       <div className="grid grid-cols-2 gap-4">
         <Card>
           <CardHeader>
@@ -40,12 +66,15 @@ const Dashboard = () => {
         <Button>Create Link</Button>
       </div>
       <div className="relative">
-      <Input type="text" placeholder="Filter Links..."
-      value={searchQuery}
-      onChange={(e)=>setSearchQuery(e.target.value)}
-      />
-      <Filter className="absolute top-2 right-2"/>
+        <Input
+          type="text"
+          placeholder="Filter Links..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <Filter className="absolute top-2 right-2" />
       </div>
+      {error && <Error message={error?.message} />}
     </div>
   );
 };
